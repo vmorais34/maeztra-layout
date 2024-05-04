a713d73156d24e045a0c2e9e1a23bce1c8bf2db4
Vinicius Morais <vinicius@benova.ag>
2024-05-04 11:10:17 -0300

    update: colors and media queries screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1026px',
+      xl: '1440px',
+    },
+    colors: {
+      base: '#FAA500',
+      primary: '#FFFFFF',
+      secondary: '#353535',
+      tertiary: '#EFEFEF',
+      textPrimary: '#353535',
+      textSecondary: '#FFFFFF'
+    },
     extend: {},
   },
   plugins: [],
